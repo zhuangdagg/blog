70816d8de515995df89ed05a40b61275835b8f1f
chenguangzhuang <chenguangzhuang@techpci.com>
2023-06-09 18:56:43 +0800

    feat: learn hao to write;

--- .vitepress/config/index.ts
+++ .vitepress/config/index.ts
@@ -2,5 +2,50 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
     title: 'blog',
-    base: '/blog/'
+    base: '/blog/',
+    locales: {
+        root: {
+            label: '简体中文',
+            lang: 'zh-CN',
+            link: '/blog/'
+        }
+    },
+    themeConfig: {
+        outline: {
+            label: '本页内容',
+        },
+        docFooter: {
+            prev: '上一页',
+            next: '下一页',
+        },
+        nav: [
+            {
+                text: '关于',
+                link: '/blog/',
+                activeMatch: '^/',
+            }
+        ],
+        sidebar: {
+            '/': [
+                {
+                    text: '目录',
+                    items: [
+                      {
+                        text: '语法',
+                        link: '/src/js/grammar.html',
+                      },
+                    ],
+                },
+            ],
+            '/src/': [
+                {
+                    text: 'js',
+                    collapsed: true,
+                    items: [
+                        { text: '变量', link: '/' }
+                    ]
+                }
+            ]
+        }
+    }
 })
--- index.md
+++ index.md
@@ -1,1 +1,17 @@
-## blog for work
+---
+home: true
+title: 666
+features:
+  - title: Simplicity First
+    details: Minimal setup with markdown-centered project structure helps you focus on writing.
+  - title: Vue-Powered
+    details: Enjoy the dev experience of Vue + webpack, use Vue components in markdown, and develop custom themes with Vue.
+  - title: Performant
+    details: VitePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
+footer: MIT Licensed | Copyright © 2019-present Evan You
+---
+
+# 目录
+## [语法](/src/js/grammar.md)
+
+aaa
