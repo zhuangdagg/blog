e0cd2c6a1372f17c163de4804865abdf6698fcc2
chengz <1223445257@qq.com>
2025-01-16 14:06:43 +0800

    fix: leetcode;

--- docs/code/leetcode/2025-01-16.js
+++ docs/code/leetcode/2025-01-16.js
@@ -3,18 +3,25 @@
  * @date 2025-01-16
  */
 var minimumSubarrayLength = function (nums, k) {
-  let sum = 0, res = 0;
-  nums.sort((a, b) => b - a)
-  for(const n of nums) {
-    sum |= n
-    res++
-    console.log({ sum, res })
-    if(sum >= k) return res
+  let s = 0, res = Number.MAX_SAFE_INTEGER, l=0, r = 0, n = nums.length;
+  while(r < n) {
+    s |= nums[r]
+    if(s >= k) {
+      console.log({ l, r, s, res})
+      res = Math.min(r-l+1, res)
+      if(l < r) {
+        s = nums.slice(++l, r+1).reduce((s, i) => s|i, 0)
+      } else {
+        return res
+      }
+    } else {
+      r++
+    }
+    
   }
-
-  return -1
+  return res >= Number.MAX_SAFE_INTEGER ? -1 : res
 }
 
-console.assert(minimumSubarrayLength([1,2, 3], 2) === 1, "[1,2, 3] no pass")
+console.assert(minimumSubarrayLength([16,1,2,20,32], 45) === 2, "[1,2, 3] no pass")
 console.assert(minimumSubarrayLength([2, 1, 8],  10) === 3, "[2, 1, 8] no pass")
 console.log("Accepted!")
